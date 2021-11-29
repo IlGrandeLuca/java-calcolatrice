0a81d5bda73d7ae216753187c159f92f5edd1f4c
IlGrandeLuca <lucailgrande96@outlook.it>
2021-11-29 17:43:17 +0100

    Create CalcoliHelperBonus.java

---
+++ src/org/generation/italy/utils/CalcoliHelperBonus.java
@@ -0,0 +1,25 @@
+package org.generation.italy.utils;
+
+public class CalcoliHelperBonus {
+	
+	public static double calculatePower(double num, int exponent) {
+		double res = 1;
+		if (num > 0 && exponent == 0) {
+			return res;
+		} else if (num == 0 && exponent >= 1) {
+			return 0;
+		} else if (exponent < 0) {
+			num = 1 / num;
+			exponent = -exponent;
+			for (int i = 1; i <= exponent; i++) {
+				res *= num;
+			}
+			return res;
+		} else {
+			for (int i = 1; i <= exponent; i++) {
+				res *= num;
+			}
+			return res;
+		}
+	}
+}
